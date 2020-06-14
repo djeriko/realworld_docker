5c3686daaeb60cdbe685d3c787060db83b441398
djeriko <jerikosalivan@gmail.com>
2020-06-14 18:51:43 +0900

    added mongodb

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,21 @@
-const express = require('express')
+const express = require("express")
+const { connectDb } = require("./helpers/db")
+const { host, port, db } = require("./configuration")
 const app = express()
-const port = process.env.PORT
-const host = process.env.HOST
 
-app.get('/test', (req, res) => {
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api service on port: ${port}`)
+    console.log(`On host ${host}`)
+    console.log(`Our database ${db}`)
+  })
+}
+
+app.get("/test", (req, res) => {
   res.send("Our api sercer is working correctly")
 })
 
-app.listen(port, ()=> {
-  console.log(`Started api service on port: ${port}`)
-  console.log(`On host ${host}`)
-})
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer)
